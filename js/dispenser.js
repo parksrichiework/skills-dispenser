ee99640c7152956ab5f270b73199cb8383f64386
Richie Parks <parksrichiework@gmail.com>
2022-01-05 17:55:24 -0600

    added css and html

---
+++ js/dispenser.js
@@ -0,0 +1,12 @@
+var changeState = function(state) {
+	document.body.className = 'body-state'+state;
+
+function Array() {
+    var a = [];
+    for (var i = 0; i < /* arguments.length = */ N; i++) {
+        a.push(/* arguments[i] = */ undefined);
+    }
+    return a;
+}	
+		
+
